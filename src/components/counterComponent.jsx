7569200acb8d0f02b0bc2f761a6cc8de7f4fb37f
Saarang Tiwari <saarang.tiwari@hotmail.com>
2019-06-13 13:28:57 +0530

    added color based badge classes

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  // state = {  }
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
+  };
+
   render() {
+    let countClass = "badge m-2 badge-";
+    countClass += this.state.count === 0 ? "warning" : "primary";
     return (
-      <div>
-        <h1>Hello World</h1>
-        <button>Increment</button>
-      </div>
+      <React.Fragment>
+        {/* ghost div */}
+        <span className={countClass}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
